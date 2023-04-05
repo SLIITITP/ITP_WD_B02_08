244df7b867ebf9b0dd4493ce8472ba094f74c13e
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-05 17:27:41 +0530

    Created some relevant files to payment process

---
+++ backend/models/Payments.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  studentId: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  month: {
+    type: String,
+    required: true,
+  },
+  subjects: {
+    type: [String],
+    required: true,
+  },
+  grade: {
+    type: String,
+    required: true,
+  },
+  paidAmount: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Payment = mongoose.model('Payment', paymentSchema);
+
+module.exports = Payment;
