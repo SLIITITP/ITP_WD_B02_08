87ff69503cc60187eec29ade4e251921960abc29
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-06 21:32:52 +0530

    Implementing Search by ID or Name

--- backend/routes/Payment.js
+++ backend/routes/Payment.js
@@ -6,7 +6,7 @@ const Payment = require('../models/Payments');
 router.post('/add', (req, res) => {
   const { studentId, date, month, subjects, grade, paidAmount } = req.body;
 
-  const newPayment = new Payment({ studentId, date, month, subjects, grade, paidAmount});
+  const newPayment = new Payment({ studentId, date, month, subjects, grade, paidAmount });
 
   newPayment.save()
     .then((payment) => {
@@ -15,6 +15,22 @@ router.post('/add', (req, res) => {
     .catch((error) => {
       res.status(500).json({ error: error.message });
     });
+});
+
+//Search payment using student ID
+
+router.get('/history/:studentId', async (req, res) => {
+  try {
+    const studentId = req.params.studentId;
+    const payments = await Payment.find({ studentId: studentId });
+    if (payments.length === 0) {
+      return res.status(404).json({ message: 'No payment found for student ID ' + studentId });
+    }
+    res.json(payments);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
--- frontend/src/components/AddPayment.js
+++ frontend/src/components/AddPayment.js
@@ -1,14 +1,54 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function AddPayment() {
-    
+function SearchStudents() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-    return (
-        <div className='container'>
-            ABCDD
-        </div >
-    );
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const search = async () => {
+      try {
+        const response = await axios.get(`/api/nipunusers/${searchTerm}`);
+        setSearchResults([response.data]);
+      } catch (err) {
+        setSearchResults([]);
+      }
+    };
+
+    if (searchTerm.length > 0) {
+      search();
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+        placeholder="Search by ID or Name"
+      />
+
+      {searchResults.length === 0 ? (
+        <p>No results found.</p>
+      ) : (
+        <ul>
+          {searchResults.map((result) => (
+            <li key={result._id}>
+              <p>{`ID: ${result.studentID}`}</p>
+              <p>{`Name: ${result.name}`}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default AddPayment;
+export default SearchStudents;
