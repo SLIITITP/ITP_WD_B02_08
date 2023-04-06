6a0941e4ed2542a062652181cc596d36e6f5de8c
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-06 13:39:17 +0530

    my parts  - Nipun

--- backend/routes/Subject.js
+++ backend/routes/Subject.js
@@ -4,14 +4,35 @@ const Subject = require('../models/Subjects');
 
 // POST /api/subjects
 router.post('/add', async (req, res) => {
-  try {
-    const { subjectName, subjectID, subjectAmount, subjectTeacherID } = req.body;
-    const newSubject = new Subject({ subjectName, subjectID, subjectAmount, subjectTeacherID });
-    await newSubject.save();
-    res.status(201).json(newSubject);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+    try {
+        const { subjectName, subjectID, subjectAmount, subjectTeacherID } = req.body;
+        const newSubject = new Subject({ subjectName, subjectID, subjectAmount, subjectTeacherID });
+        await newSubject.save();
+        res.status(201).json(newSubject);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
+
+// Update a subject by subjectID
+router.put('/update/:subjectID', async (req, res) => {
+    try {
+        const subject = await Subject.findOneAndUpdate({ subjectID: req.params.subjectID }, req.body, { new: true });
+        res.json(subject);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+//get list of subject data
+router.get('/subjects', async (req, res) => {
+    try {
+      const subjects = await Subject.find();
+      res.json(subjects);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  });
 
 module.exports = router;
