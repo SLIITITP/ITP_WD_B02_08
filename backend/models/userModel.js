826b83413e92ad21f0f61d3a8174797be446b72e
IT21786464 <IT21786464@my.sliit.lk>
2023-04-08 18:54:48 +0530

    user,admin seperate login done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     isAdmin:{
         type: Boolean,
         required: true,
-        default: false,
+        default: true,
     }
 },{
     timestamps:true,
--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,9 +1,11 @@
-import React, { useEffect , useState} from 'react';
-import { getUserInfo } from '../apicalls/users';
-import { message } from 'antd';
+
+import { message } from "antd";
+import React, { useEffect, useState } from "react";
+import { getUserInfo } from "../apicalls/users";
+import { useDispatch, useSelector } from "react-redux";
+import { SetUser } from "../redux/usersSlice.js";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
-import { SetUser } from '../redux/usersSlice.js';
+import { HideLoading, ShowLoading } from "../redux/loaderSlice";
 import '../stylesheets/layout.css'
 import '../stylesheets/theme.css'
 import '../stylesheets/alignments.css'
@@ -11,74 +13,181 @@ import '../stylesheets/textelements.css'
 import '../stylesheets/custom-component.css'
 import '../stylesheets/form-elements.css'
 
-export default function ProtectedRoute({children}) {
+function ProtectedRoute({ children }) {
+  const { user } = useSelector((state) => state.users);
+  const [menu, setMenu] = useState([]);
+  const [collapsed, setCollapsed] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [load,setLoader] = useState(true);
- 
-  const getUserData = async() =>{
+
+  const userMenu = [
+    {
+      title: "Home",
+      paths: ["/exams", "/user/write-exam"],
+      icon: <i className="ri-home-line"></i>,
+      onClick: () => navigate("/exams"),
+    },
+    {
+      title: "Reports",
+      paths: ["/user/reports"],
+      icon: <i className="ri-bar-chart-line"></i>,
+      onClick: () => navigate("/user/reports"),
+    },
+    {
+      title: "Profile",
+      paths: ["/profile"],
+      icon: <i className="ri-user-line"></i>,
+      onClick: () => navigate("/profile"),
+    },
+    {
+      title: "Logout",
+      paths: ["/logout"],
+      icon: <i className="ri-logout-box-line"></i>,
+      onClick: () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+      },
+    },
+  ];
+
+  const adminMenu = [
+    {
+      title: "Home",
+      paths: ["/exams", "/user/write-exam"],
+      icon: <i className="ri-home-line"></i>,
+      onClick: () => navigate("/exams"),
+    },
+    {
+      title: "Exams",
+      paths: ["/admin/exams", "/admin/exams/add"],
+      icon: <i className="ri-file-list-line"></i>,
+      onClick: () => navigate("/admin/exams"),
+    },
+    {
+      title: "Reports",
+      paths: ["/admin/reports"],
+      icon: <i className="ri-bar-chart-line"></i>,
+      onClick: () => navigate("/admin/reports"),
+    },
+    {
+      title: "Profile",
+      paths: ["/profile"],
+      icon: <i className="ri-user-line"></i>,
+      onClick: () => navigate("/profile"),
+    },
+    {
+      title: "Logout",
+      paths: ["/logout"],
+      icon: <i className="ri-logout-box-line"></i>,
+      onClick: () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+      },
+    },
+  ];
+
+  const getUserData = async () => {
     try {
-      const response = await getUserInfo()
-     
-      if(response.success){
-        message.success(response.message)
-        console.log(response);
-        dispatch(SetUser(response.data))
-        
-      
-      }else{
-        message.error(response.message)
+      dispatch(ShowLoading());
+      const response = await getUserInfo();
+      dispatch(HideLoading());
+      if (response.success) {
+        dispatch(SetUser(response.data));
+        if (response.data.isAdmin) {
+          setMenu(adminMenu);
+        } else {
+          setMenu(userMenu);
+        }
+      } else {
+        message.error(response.message);
       }
     } catch (error) {
-      message.error(error.message)
+      navigate("/login");
+      dispatch(HideLoading());
+      message.error(error.message);
     }
-  }
-  const fetchUser = () => {
-    return async dispatch => {
-      const response = await getUserInfo();
-      console.log(response);
-      setLoader(false)
-      const user = response.data
-      user.password = null
-      localStorage.setItem("user" , JSON.stringify(user))
-      dispatch(SetUser(response.data))
-
-      
-    };
   };
-  
 
-  useEffect(()=>{
+  useEffect(() => {
     if (localStorage.getItem("token")) {
-      dispatch(fetchUser());
+      getUserData();
     } else {
       navigate("/login");
     }
-  }, [dispatch])
+  }, []);
 
-  const {user}  = useSelector((state) =>  {console.log(state.users)
-    return state.users
-  });
-  
+  const activeRoute = window.location.pathname;
+
+  const getIsActiveOrNot = (paths) => {
+    if (paths.includes(activeRoute)) {
+      return true;
+    } else {
+      if (
+        activeRoute.includes("/admin/exams/edit") &&
+        paths.includes("/admin/exams")
+      ) {
+        return true;
+      }
+      if (
+        activeRoute.includes("/user/write-exam") &&
+        paths.includes("/user/write-exam")
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   return (
-    
-    <div className='layout'>
-      <div className="flex gap-2">
+    <div className="layout">
+      <div className="flex gap-2 w-full h-full h-100">
         <div className="sidebar">
-          Sidebar
+          <div className="menu ">
+            {menu.map((item, index) => {
+               return (
+                <div
+                  className={`menu-item ${
+                    getIsActiveOrNot(item.paths) && "active-menu-item"
+                  }`}
+                  key={index}
+                  onClick={item.onClick}
+                >
+                  {item.icon}
+                  {!collapsed && <span>{item.title}</span>}
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className="body">
           <div className="header flex justify-between">
-            Header
+            {!collapsed && (
+              <i
+                className="ri-close-line"
+                onClick={() => setCollapsed(true)}
+              ></i>
+            )}
+            {collapsed && (
+              <i
+                className="ri-menu-line"
+                onClick={() => setCollapsed(false)}
+              ></i>
+            )}
+            <h1 className="text-2xl text-white">Thilina Institute</h1>
+            <div>
+              <div className="flex gap-1 items-center">
+                <i class="ri-user-line"></i>
+                <h1 className="text-md text-white underline">{user?.name}</h1>
+              </div>
+              <span>Role : {user?.isAdmin ? "Admin" : "User"}</span>
+            </div>
           </div>
-          <div className="content">
-          {user?.name} 
-            {children}
-          </div>
+          <div className="content">{children}</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export default ProtectedRoute;
 
