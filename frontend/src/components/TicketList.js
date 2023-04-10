cc7052c95290b90d4babf266744f25566142fb11
it21273094 <it21273094@my.sliit.lk>
2023-04-10 19:47:03 +0530

    update frontend ui

--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -61,10 +61,10 @@ export default class TicketList extends Component{
  render(){
   return(
     <div className="container">
-      <div className="row">
-        <div className="col-lg-9 mt-2 mb-2">
-            <h4> Tickets List </h4>
-        </div>
+    <div className="row">
+      <div className="col-lg-9 mt-2 mb-2">
+      <h3 className="text-3xl font-bold dark:text-white" style={{marginLeft:'20px'}}> Ticket List</h3>
+      </div>
 
         
         <div className="col-lg-3 mt-2 mb-3">
@@ -79,26 +79,42 @@ export default class TicketList extends Component{
         </div>
       </div>
 
-      <div><center><h4> Total Tickets:  {this.state.tickets.length}</h4>
-</center>
-        </div>
-   
-          <table className="table table-hover" style={{marginTop:'40px'}}>
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Reg.Number</th>
-                <th scope="col">Subject</th>
-                <th scope="col">Status</th>
-                <th scope="col">Open Date</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.tickets.map((tickets,index)=>(
-                <tr key={index}>
-                  <th scope="row">{index+1}</th>
-                  <td>
+      <div><center>
+        <h4  class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
+       <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500"> Total Tickets:  {this.state.tickets.length}</mark></h4>
+      </center></div>
+
+<br></br>
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Reg.Number
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Subject
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Open Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+      
+            {this.state.tickets.map((tickets,index)=>(
+                <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <th scope="row"  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{index+1} </th>
+                  <td class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
                     <a href={ `/reply/${tickets._id}`} style={{textDecoration:'none'}}>
                     {tickets.Rnumber}
                     </a>
@@ -107,20 +123,20 @@ export default class TicketList extends Component{
                   <td>{tickets.status}</td>
                   <td>{tickets.openAt}</td>
                   <td> 
-                    <a className="btn btn-danger" href="#" onClick={()=>this.onDelete(tickets._id)}>
-                      <i className="far fa-trash-alt"></i>&nbsp;Delete
-                    </a>
-                  </td>
+                   
+                    
 
-   
-
-                </tr>
-
+            <a class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+             href="#" onClick={()=>this.onDelete(tickets._id)}>Delete</a>
+         
+                   
                 
-
-              ))}
-            </tbody>
-          </table>
+                </td>
+            </tr>
+            ))}
+        </tbody>
+    </table>
+</div>
         
         
 
