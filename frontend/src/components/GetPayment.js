1a9a7ffa837730a3ad1a4eafed96720e83e067e8
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-10 14:42:54 +0530

    Added a comment to check

--- frontend/src/components/GetPayment.js
+++ frontend/src/components/GetPayment.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../stylesheets/Payment.css'
 
+//Added a comment to check
 function GetPayment() {
     //set values
     const [studentId, setStudentId] = useState('');
