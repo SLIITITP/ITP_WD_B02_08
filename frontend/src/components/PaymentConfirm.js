f9648b8697c6e2759752919d9ad166ddc6723bd9
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-11 15:55:18 +0530

    added payment confirmation page

---
+++ frontend/src/components/PaymentConfirm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../stylesheets/Payment.css'
+
+export default function PaymentConfirm() {
+
+
+
+  return (
+    <div>
+        <h3>Payment Confirmation</h3>
+        <div className='get-center'>
+            Your Payment has recieved
+        </div>
+
+    </div>
+  )
+}
