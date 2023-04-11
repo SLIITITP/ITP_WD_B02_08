2c79f0eeb2eb6bbf2913ec8c109c0f091b30c6ea
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-11 16:53:09 +0530

    installed stripe to backend

--- frontend/src/components/PaymentConfirm.js
+++ frontend/src/components/PaymentConfirm.js
@@ -5,13 +5,24 @@ export default function PaymentConfirm() {
 
 
 
-  return (
-    <div>
-        <h3>Payment Confirmation</h3>
-        <div className='get-center'>
-            Your Payment has recieved
+    return (
+        <div>
+            <h3>Payment Confirmation</h3>
+            <div className='get-center'>
+                <div>
+                    Your Payment has recieved<br />
+                    <label>
+                        Student ID : <br />
+                        Student Name : <br />
+                        Month : <br />
+                        Total Amount : <br />
+                        Paid Subjects : <br />
+                        Payment ID : <br />
+                    </label>
+                    <br /><button>Back to Home</button>
+                    <br /><button>Download Payment Reciept</button>
+                </div>
+            </div>
         </div>
-
-    </div>
-  )
+    )
 }
