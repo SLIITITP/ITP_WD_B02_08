63d94bf2d3771afe53e888eaac1a309032f25572
IT21786464 <IT21786464@my.sliit.lk>
2023-04-15 11:15:42 +0530

    awurudu special

--- frontend/src/pages/user/UserReports/UserReports.js
+++ frontend/src/pages/user/UserReports/UserReports.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PageTitle from "../../../components/PageTitle";
-import { message, Modal, Table } from "antd";
+import { message, Modal, Table , Row , Col} from "antd";
 import { useDispatch } from "react-redux";
 import { HideLoading, ShowLoading } from "../../../redux/loaderSlice";
 import { getAllReportsByUser } from "../../../apicalls/reports";
 import { useEffect } from "react";
 import moment from "moment";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
 
 function UserReports() {
   const [reportsData, setReportsData] = React.useState([]);
@@ -44,6 +46,7 @@ function UserReports() {
       render: (text, record) => <>{record.result.verdict}</>,
     },
   ];
+  
 
   const getData = async () => {
     try {
@@ -65,11 +68,63 @@ function UserReports() {
     getData();
   }, []);
 
+  const downloadPDF = (report) => {
+    const doc = new jsPDF();
+    doc.autoTable({
+      startY: 20,
+      head: [["Thilina Institute", ""]],
+      body: [
+        ["Name", report.users?.name],
+        ["Subject", report.exam.category],
+        ["Date", moment(report.createdAt).format("DD-MM-YYYY hh:mm:ss")],
+        ["Duration", report.exam.duration],
+        ["Total Marks", report.exam.totalMarks],
+        ["Passing Marks", report.exam.passingMarks],
+        ["Obtained Mark", report.result.correctAnswers.length],
+        ["Verdict", report.result.verdict],
+      ],
+    });
+    doc.save(`${report.exam.name}.pdf`);
+  };
+
   return (
     <div>
       <PageTitle title="Reports" />
       <div className="divider"></div>
       <Table columns={columns} dataSource={reportsData} />
+      <Row gutter={[10, 10]}>
+        {reportsData.map((report) => (
+          <Col span={8} key={report.id}>
+            <div className="card flex flex-col gap-2 p-2 mt-10">
+              <h1 className="text-2xl">
+                <b>{report.exam.name}</b>
+              </h1> {/*show exam details */}
+              <hr />
+              <h1 className="text-md">Name : {report.users?.name}</h1>
+              <h1 className="text-md">Subject : {report.exam.category}</h1>
+              <h1 className="text-md">
+                Date : {moment(report.createdAt).format("DD-MM-YYYY hh:mm:ss")}{" "}
+              </h1>
+              <h1 className="text-md">Duration : {report.exam.duration} </h1>
+              <h1 className="text-md">Total Marks : {report.exam.totalMarks}</h1>
+              <h1 className="text-md">
+                Passing Marks : {report.exam.passingMarks}
+              </h1>
+              <h1 className="text-md">
+                Obtained Mark : {report.result.correctAnswers.length}
+              </h1>
+              <h1 className="text-md">Verdict :{report.result.verdict}</h1>
+
+              <button
+                className="primary-outlined-btn"
+                onClick={() => downloadPDF(report)}
+              >
+                Print Result
+              </button>
+            </div>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
