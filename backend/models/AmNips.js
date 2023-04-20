90e53b335bed9ac1e3e21c3c5d16c2d32b3f9b7f
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-20 20:17:13 +0530

    AmNip Implementing

--- backend/models/AmNips.js
+++ backend/models/AmNips.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const amNipSchema = new Schema({
-  date: {type: Date, required: true},
-  time: {type: String, required: true},
-  subject: {type: Schema.Types.ObjectId, ref: 'Subject', required: true},
-  student: {type: Schema.Types.ObjectId, ref: 'NipunUser', required: true}
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    subject: { type: Schema.Types.ObjectId, ref: 'Subject', required: true },
+    teacher: { type: Schema.Types.String, ref: 'Subject', required: true },
+    student: { type: Schema.Types.ObjectId, ref: 'NipunUser', required: true },
+    studentID: { type: Schema.Types.String, ref: 'NipunUser', required: true },
+    grade: { type: String, required: true }
 });
 
 const AmNip = mongoose.model('AmNip', amNipSchema);
