9d2140def1f62834c40ccd42ef470f049f85ffe6
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-04-25 14:36:04 +0530

    QR modified

--- frontend/src/components/PaymentComponents/ViewPayment.js
+++ frontend/src/components/PaymentComponents/ViewPayment.js
@@ -321,26 +321,29 @@ function ViewPayment() {
                         {payments.map((payment) => (
                             <tr key={payment._id}>
                                 <td className="flex items-center border px-4 py-4">
-                                    <span className="mr-2">{payment.studentId}{payment.notice}</span>
+                                    <span className="mr-2">{payment.studentId}</span>
                                     {payment.notice && (
                                         <>
                                             <div className="inline-block cursor-pointer" onClick={() => setIsOpen(true)}>
                                                 <InformationCircleIcon className="w-5 h-5 text-blue-500" />
                                             </div>
                                             {isOpen && (
-                                                <div className="fixed inset-0 z-50 flex items-center justify-center">
-                                                    <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
-                                                    <div className="z-50 w-1/2 max-w-md p-6 bg-white rounded-lg shadow-lg">
-                                                        <h2 className="text-lg font-medium mb-4">Payment Notice</h2>
-                                                        <p className="mb-4">{payment.notice}</p>
-                                                        <button
-                                                            className="px-4 py-2 font-medium text-white bg-blue-500 rounded hover:bg-blue-600"
-                                                            onClick={() => setIsOpen(false)}
-                                                        >
-                                                            Close
-                                                        </button>
-                                                    </div>
-                                                </div>
+                                                // <div className="fixed inset-0 z-50 flex items-center justify-center">
+                                                //     <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
+                                                //     <div className="z-50 w-1/2 max-w-md p-6 bg-white rounded-lg shadow-lg">
+                                                //         <h2 className="text-lg font-medium mb-4">Payment Notice</h2>
+                                                //         <p className="mb-4">{payment.notice}</p>
+                                                //         <button
+                                                //             className="px-4 py-2 font-medium text-white bg-blue-500 rounded hover:bg-blue-600"
+                                                //             onClick={() => setIsOpen(false)}
+                                                //         >
+                                                //             Close
+                                                //         </button>
+                                                //     </div>
+                                                // </div>
+
+                                                <a onClick={() => setIsOpen(false)}><p>{payment.notice}</p></a>
+
                                             )}
                                         </>
                                     )}
