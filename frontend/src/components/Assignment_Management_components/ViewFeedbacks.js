e6badfabc74575d8282eba0acb1ec4038a7e6df1
Kavindi Fernando <123950324+Kavindi-01@users.noreply.github.com>
2023-04-26 15:08:59 +0530

    edited

--- frontend/src/components/Assignment_Management_components/ViewFeedbacks.js
+++ frontend/src/components/Assignment_Management_components/ViewFeedbacks.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Table, Button, Form } from 'react-bootstrap';
+import back from '../../assets/MaterialBg.jpg';
 
 const ViewFeedbacks = () => {
     const [feedbacks, setFeedbacks] = useState([]);
@@ -30,6 +31,10 @@ const ViewFeedbacks = () => {
 
     return (
         <div className="container">
+
+<div className="opacity-50 absolute" style={{ zIndex: -1 }}>
+        <img src={back} alt="logo" />
+      </div>
             
             <h1 className="text-center my-5" style={{ fontSize: '2rem' }}>
             View Feedbacks
@@ -38,32 +43,33 @@ const ViewFeedbacks = () => {
 
 <Form inline striped bordered hover style={{ marginTop: '50px' }}>
                 <Form.Control type="text" placeholder="Search by Teacher's Name" className="mr-sm-2"
-                    value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+                    value={searchTerm} onChange={e => setSearchTerm(e.target.value)}  style={{ marginBottom: "5rem" }}/>
                
             </Form>
 
             
 
-            <Table striped bordered hover style={{ marginTop: '50px' }}>
-                <thead>
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400" style={{ backgroundColor: 'transparent',marginBottom: "1.5rem" }}  >
+            
+                <thead  className="text-xs text-white uppercase bg-black dark:bg-gray-700 dark:text-gray-400" >
                     <tr>
-                        <th>Teacher's Name</th>
-                        <th>Grade</th>
-                        <th>Assignment Type</th>
-                        <th>Email</th>
-                        <th>Message</th>
-                        <th>Response</th>
+                        <th th scope="col" className="px-6 py-3">Teacher's Name</th>
+                        <th th scope="col" className="px-6 py-3">Grade</th>
+                        <th th scope="col" className="px-6 py-3">Assignment Type</th>
+                        <th th scope="col" className="px-6 py-3">Email</th>
+                        <th th scope="col" className="px-6 py-3">Message</th>
+                        <th th scope="col" className="px-6 py-3">Response</th>
                     </tr>
                 </thead>
                 <tbody>
                     {filteredFeedbacks.map((feedback, index) => (
-                        <tr key={index}>
-                            <td>{feedback.teachersName}</td>
-                            <td>{feedback.grade}</td>
-                            <td>{feedback.assignmentType}</td>
-                            <td>{feedback.email}</td>
-                            <td>{feedback.message}</td>
-                            <td>
+                        <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td className="px-6 py-4">{feedback.teachersName}</td>
+                            <td className="px-6 py-4">{feedback.grade}</td>
+                            <td className="px-6 py-4">{feedback.assignmentType}</td>
+                            <td className="px-6 py-4">{feedback.email}</td>
+                            <td className="px-6 py-4">{feedback.message}</td>
+                            <td className="px-6 py-4">
                                 <button onClick={() => handleReply(feedback)}
                                     className="btn btn-primary">
                                    Reply
@@ -72,7 +78,7 @@ const ViewFeedbacks = () => {
                         </tr>
                     ))}
                 </tbody>
-            </Table>
+            </table>
         </div>
     );
 };
