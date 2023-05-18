3adfd4ab739ef1d1047dc362264d0fb379df517b
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-05-18 09:42:25 +0530

    chart updated

--- frontend/src/components/Assignment_Management_components/ChartJsExample.js
+++ frontend/src/components/Assignment_Management_components/ChartJsExample.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { Bar } from 'react-chartjs-2';
+import { Chart } from 'chart.js/auto';
 import { getProfile } from '../../apicalls/helper';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
@@ -37,83 +37,48 @@ const ChartJsExample = () => {
     const filteredMarks = marks1.filter((marks) => marks.studentName === id);
     if (filteredMarks.length > 0) {
       setMarks(filteredMarks);
+      console.log(filteredMarks);
     }
   }, [marks1, id]);
+
+  useEffect(() => {
+    if (marks.length > 0) {
+      createChart();
+    }
+  }, [marks]);
 
   const createChart = () => {
     const assignmentTypes = marks.map((mark) => mark.AssignmentType);
     const markValues = marks.map((mark) => mark.marks);
 
-    const chartData = {
-      labels: assignmentTypes,
-      datasets: [
-        {
-          label: 'Marks',
-          data: markValues,
-          backgroundColor: 'rgba(75, 192, 192, 0.6)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-        },
-      ],
-    };
-
-    const chartOptions = {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        y: {
-          beginAtZero: true,
-          max: 100,
+    const chartConfig = {
+      type: 'bar',
+      data: {
+        labels: assignmentTypes,
+        datasets: [
+          {
+            label: 'Marks',
+            data: markValues,
+            backgroundColor: 'rgba(75, 192, 192, 0.6)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+            max: 100,
+          },
         },
       },
     };
 
-    return (
-      <div>
-        <div style={{ width: '300px', height: '200px' }}>
-          <Bar ref={chartRef} data={chartData} options={chartOptions} />
-        </div>
-        <button onClick={printChart}>Print</button>
-      </div>
-    );
-  };
-
-  const printChart = () => {
-    if (chartRef.current && chartRef.current.chartInstance) {
-      const chartCanvas = chartRef.current.chartInstance.canvas;
-      const printWindow = window.open('', '_blank');
-      printWindow.document.open();
-      printWindow.document.write(`
-        <html>
-          <head>
-            <title>Print Chart</title>
-            <style>
-              @media print {
-                body * {
-                  visibility: hidden;
-                }
-                #chart-container, #chart-container * {
-                  visibility: visible;
-                }
-                #chart-container {
-                  position: absolute;
-                  left: 0;
-                  top: 0;
-                  width: 100%;
-                  height: 100%;
-                }
-              }
-            </style>
-          </head>
-          <body>
-            <div id="chart-container">${chartCanvas.outerHTML}</div>
-          </body>
-        </html>
-      `);
-      printWindow.document.close();
-      printWindow.onload = () => {
-        printWindow.print();
-      };
+    if (chartRef.current) {
+      new Chart(chartRef.current, chartConfig);
     }
   };
 
@@ -141,8 +106,25 @@ const ChartJsExample = () => {
       <div style={{ width: '300px', height: '200px' }}>
         <canvas ref={chartRef} />
       </div>
+      <table ref={tableRef}>
+        <thead>
+          <tr>
+            <th>Assignment Type</th>
+            <th>Marks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {marks.map((mark, index) => (
+            <tr key={index}>
+              <td>{mark.AssignmentType}</td>
+              <td>{mark.marks}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={generatePDF}>Generate PDF</button>
     </div>
-    );
-    };
-    
-    export default ChartJsExample;
+  );
+};
+
+export default ChartJsExample;
