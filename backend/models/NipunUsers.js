7d226c0bab68c4c53ba084a84fdb88b9b3d1837c
Nipun Wijethunge <it21559136@my.sliit.lk>
2023-05-19 08:35:56 +0530

    my code

--- backend/models/NipunUsers.js
+++ backend/models/NipunUsers.js
@@ -12,15 +12,15 @@ const nipunUserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true
+    required: false
   },
   grades: {
     type: [String],
-    required: true
+    required: false
   },
   whatsappNumber: {
       type: String,
-      required: true
+      required: false
   },
   phoneNumber: {
       type: String,
@@ -28,7 +28,7 @@ const nipunUserSchema = new mongoose.Schema({
   },
   address: {
       type: String,
-      required: true
+      required: false
   },
   password: {
       type: String,
@@ -36,43 +36,43 @@ const nipunUserSchema = new mongoose.Schema({
   }
 });
 
-nipunUserSchema.pre('save', async function (next) {
-  const now = new Date();
-  const year = now.getFullYear().toString().substr(-2);
-  const stdPrefix = 'STD';
-  let grade;
-  const lastGrade = this.grades[this.grades.length - 1];
-  if (lastGrade === "Other") {
-    grade = 0;
-  } else {
-    grade = lastGrade;
-  }
-  let sequenceNum = 1;
-  let idWithoutSeq = '';
-  let isUnique = false;
+// nipunUserSchema.pre('save', async function (next) {
+//   const now = new Date();
+//   const year = now.getFullYear().toString().substr(-2);
+//   const stdPrefix = 'STD';
+//   let grade;
+//   const lastGrade = this.grades[this.grades.length - 1];
+//   if (lastGrade === "Other") {
+//     grade = 0;
+//   } else {
+//     grade = lastGrade;
+//   }
+//   let sequenceNum = 1;
+//   let idWithoutSeq = '';
+//   let isUnique = false;
 
-  while (!isUnique) {
-    const sequenceCode = sequenceNum.toString().padStart(4, '0');
-    idWithoutSeq = stdPrefix + year + grade.toString() + sequenceCode;
+//   while (!isUnique) {
+//     const sequenceCode = sequenceNum.toString().padStart(4, '0');
+//     idWithoutSeq = stdPrefix + year + grade.toString() + sequenceCode;
 
-    // check if the generated student ID already exists in the database
-    const existingStudent = await mongoose.model('NipunUser', nipunUserSchema).findOne({ studentID: idWithoutSeq }).exec();
-    if (!existingStudent) {
-      isUnique = true;
-    } else {
-      sequenceNum++;
-    }
-  }
+//     // check if the generated student ID already exists in the database
+//     const existingStudent = await mongoose.model('NipunUser', nipunUserSchema).findOne({ studentID: idWithoutSeq }).exec();
+//     if (!existingStudent) {
+//       isUnique = true;
+//     } else {
+//       sequenceNum++;
+//     }
+//   }
 
-  // generate random password
-  if (!this.password) {
-    const password = Math.random().toString(30).substring(2, 8);
-    //const hashedPassword = await bcrypt.hash(password, 10);
-    this.password = password;
-  }
+//   // generate random password
+//   if (!this.password) {
+//     const password = Math.random().toString(30).substring(2, 8);
+//     //const hashedPassword = await bcrypt.hash(password, 10);
+//     this.password = password;
+//   }
 
-  this.studentID = idWithoutSeq;
-  next();
-});
+//   this.studentID = idWithoutSeq;
+//   next();
+// });
 
 module.exports = mongoose.model('NipunUser', nipunUserSchema);
--- frontend/src/components/PaymentComponents/NipTest.js
+++ frontend/src/components/PaymentComponents/NipTest.js
@@ -1,30 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const ButtonPage = () => {
+const AddUserForm = () => {
+  const [user, setUser] = useState({
+    studentID: '',
+    name: '',
+    email: '',
+    grades: [],
+    whatsappNumber: '',
+    address: '',
+    // Other properties as needed
+  });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/user/add', user);
+      console.log(response.data); // New user object returned by the server
+      // Reset the form after successful submission
+      setUser({
+        studentID: '',
+        name: '',
+        email: '',
+        grades: [],
+        whatsappNumber: '',
+        address: '',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="flex flex-wrap h-screen">
-      <div className="w-1/2 p-2">
-        <button className="w-full h-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Button 1
-        </button>
-      </div>
-      <div className="w-1/2 p-2">
-        <button className="w-full h-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-          Button 2
-        </button>
-      </div>
-      <div className="w-1/2 p-2">
-        <button className="w-full h-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Button 3
-        </button>
-      </div>
-      <div className="w-1/2 p-2">
-        <button className="w-full h-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-          Button 4
-        </button>
-      </div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Student ID:
+        <input
+          type="text"
+          name="studentID"
+          value={user.studentID}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={user.name}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Grades:
+        <input
+          type="text"
+          name="grades"
+          value={user.grades}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        WhatsApp Number:
+        <input
+          type="text"
+          name="whatsappNumber"
+          value={user.whatsappNumber}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Address:
+        <input
+          type="text"
+          name="address"
+          value={user.address}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Add User</button>
+    </form>
   );
 };
 
-export default ButtonPage;
+export default AddUserForm;
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const ButtonPage = () => {
+//   return (
+//     <div className="flex flex-wrap h-screen">
+//       <div className="w-1/2 p-2">
+//         <button className="w-full h-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+//           Button 1
+//         </button>
+//       </div>
+//       <div className="w-1/2 p-2">
+//         <button className="w-full h-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+//           Button 2
+//         </button>
+//       </div>
+//       <div className="w-1/2 p-2">
+//         <button className="w-full h-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+//           Button 3
+//         </button>
+//       </div>
+//       <div className="w-1/2 p-2">
+//         <button className="w-full h-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+//           Button 4
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ButtonPage;
